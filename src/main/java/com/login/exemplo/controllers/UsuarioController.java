69f5e23cde6785192501ecf2c74000b590ba2bd2
KenpsSouza <Kenpsrossi@gmail.com>
2025-10-29 11:06:55 -0300

    get-delete-put

--- src/main/java/com/login/exemplo/controllers/UsuarioController.java
+++ src/main/java/com/login/exemplo/controllers/UsuarioController.java
@@ -1,9 +1,14 @@
 package com.login.exemplo.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import com.login.exemplo.entity.Usuario;
@@ -38,6 +43,34 @@ public class UsuarioController {
 		}
 	}
 	
+	@GetMapping(value = "/usuario/listar")
+	public ResponseEntity<?> listarUsuarios() {
+		return ResponseEntity.ok(usuarioRepository.findAll());
+	}
 	
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<?> deletar(@PathVariable Integer id) {
+		if(usuarioRepository.existsById(id)) {
+			usuarioRepository.deleteById(id);
+			return ResponseEntity.status(HttpStatus.OK).body("Excluido com sucesso"); //204
+		} else {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Esse Id não existe"); //404
+		}
+	}
+	
+	@PutMapping("/{id}")
+	public ResponseEntity<?> atualizar(@PathVariable Integer id, @RequestBody Usuario user) {
+		if(usuarioRepository.existsById(id)) {
+			Usuario usuarioAtualizado = usuarioRepository.findById(id).get();
+			usuarioAtualizado.setNome(user.getNome());
+			usuarioAtualizado.setEmail(user.getEmail());
+			usuarioAtualizado.setPassword(user.getPassword());
+			usuarioRepository.save(usuarioAtualizado);
+			return ResponseEntity.status(HttpStatus.OK).body(usuarioAtualizado); //200
+		} else {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Esse Id não existe"); //404
+		}
+	}
 
 }
